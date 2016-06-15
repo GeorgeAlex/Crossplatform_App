aa3703093b4f194145ae7068e811fc1a868a747e
gs <gs@cad-scan.co.uk>
2016-06-15 14:21:03 +0100

    Images fixed

--- Bipolar Chef/www/js/index.js
+++ Bipolar Chef/www/js/index.js
@@ -11,11 +11,11 @@ var located = 0;
 function faceWap(){
 	if (wap == 0){
 		wap = 1;
-		document.getElementById("logoDiv").style.background = "url(../img/logo2.png) no-repeat center top";
+		document.getElementById("logoDiv").style.background = "url(img/logo2.png) no-repeat center top";
 	}
 	else{
 		wap = 0;
-		document.getElementById("logoDiv").style.background = "url(../img/logo.png) no-repeat center top";
+		document.getElementById("logoDiv").style.background = "url(img/logo.png) no-repeat center top";
 	}
 	document.getElementById("logoDiv").style.backgroundSize = "100%";
 }
