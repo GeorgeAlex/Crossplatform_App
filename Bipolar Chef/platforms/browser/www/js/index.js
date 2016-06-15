18ddc16a82b7016657014e894fa82302129a3bf3
gs <gs@cad-scan.co.uk>
2016-06-15 14:06:01 +0100

    Added location function

--- Bipolar Chef/platforms/browser/www/js/index.js
+++ Bipolar Chef/platforms/browser/www/js/index.js
@@ -7,6 +7,7 @@ function loadSignUp(){
 }
 
 var wap = 0;
+var located = 0;
 function faceWap(){
 	if (wap == 0){
 		wap = 1;
@@ -17,4 +18,34 @@ function faceWap(){
 		document.getElementById("logoDiv").style.background = "url(../img/logo.png) no-repeat center top";
 	}
 	document.getElementById("logoDiv").style.backgroundSize = "100%";
+}
+
+var onSuccess = function(position) {
+        /*alert('Latitude: '          + position.coords.latitude          + '\n' +
+              'Longitude: '         + position.coords.longitude         + '\n' +
+              'Altitude: '          + position.coords.altitude          + '\n' +
+              'Accuracy: '          + position.coords.accuracy          + '\n' +
+              'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+              'Heading: '           + position.coords.heading           + '\n' +
+              'Speed: '             + position.coords.speed             + '\n' +
+              'Timestamp: '         + position.timestamp                + '\n');*/	
+			  
+		document.getElementById("Locate_DeliverButton").innerHTML = '<img src="img/deliver1.png" style="vertical-align:central"/> DELIVER'; 
+    };
+
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+
+function locate_Deliver(){
+	if (located == 0){
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		located = 1;
+	}
+	else{
+		alert("Now deliver");
+	}
 }
